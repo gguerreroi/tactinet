c4283794912f60f8a0d21c13e8d8866165a6a6e6
Gabriel Guerrero <gguerrero@sonivitac.com>
2021-12-21 12:27:45 -0600

    Controller Login Success

--- src/controllers/seguridad.controller.js
+++ src/controllers/seguridad.controller.js
@@ -4,10 +4,14 @@ import {getConnection, mssql} from "../middlewares/database";
 import {JsonOut} from "../middlewares/JsonOut";
 
 
-export async function checkUserAndPassword(user, password, db) {
+export async function checkUserAndPassword(request, response) {
+    const {username, password, database} = request.body
+
     let Connection = null
+
     try {
-        Connection = await getConnection(user, password, '45.5.118.219', db)
+
+        Connection = await getConnection(username, password, '45.5.118.219',`PLR00${database}`)
 
         if (Connection.code === 500)
             throw {code: Connection.code, message: Connection.message}
@@ -15,12 +19,13 @@ export async function checkUserAndPassword(user, password, db) {
         const query = await Connection.request()
 
         query.execute('seguridad.sp_usuarios', function (err, rows) {
-            if (!err)
-                return JsonOut('200', 'Login Succeded', rows.recordsets[0][0])
-
-            return JsonOut('500', 'Se produjo un error al ejecutar el procedimiento', err)
+            if (!err){
+                response.status(200).send(JsonOut('200', 'Login Succeded', rows.recordsets[0][0]))
+            }else{
+                response.status(500).send(JsonOut('500', 'Se produjo un error al ejecutar el procedimiento', err))
+            }
         })
     } catch (e) {
-        return JsonOut(e.code, e.message)
+        response.status(e.code).send(JsonOut(e.code, e.message))
     }
 }
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -4,18 +4,6 @@ import {JsonOut} from "../middlewares/JsonOut";
 
 const router = Router();
 
-router.post('/auth', function (request, response) {
-    const {username, password, database} = request.body
-
-    const a = Security.checkUserAndPassword(username, password, `PLR00${database}`);
-
-    Promise.all([a]).then(value => {
-        console.log("a then", value)
-        response.status(value[0].state.Code).send(value[0])
-    }).catch(reason => {
-        console.log("reason: ", reason)
-        response.status(500).send(JsonOut('500', 'Se produjo un error en la ejecucion de promesa'))
-    })
-})
+router.post('/auth', Security.checkUserAndPassword)
 
 export default router;
