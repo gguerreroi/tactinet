4e4b2b719ffb741cc56072782a4d3239ff8aaeec
Gabriel Guerrero <47202574+gguerreroi@users.noreply.github.com>
2022-07-29 03:57:56 -0600

    CHANGES

--- public/assets/js/custom/tasks/onu-activate.js
+++ public/assets/js/custom/tasks/onu-activate.js
@@ -1,22 +1,22 @@
 const TKOnuActivate = function () {
-    let div_element;
-    let stepper;
+	let div_element;
+	let stepper;
 
-    function handle() {
-        stepper = new KTStepper(div_element)
-        stepper.on("kt.stepper.next", function (stepper) {
-            stepper.goNext(); // go next step
-        });
+	function handle() {
+		stepper = new KTStepper(div_element)
+		stepper.on("kt.stepper.next", function (stepper) {
+			stepper.goNext(); // go next step
+		});
 
-        stepper.on("kt.stepper.previous", function (stepper) {
-            stepper.goPrevious(); // go previous step
-        });
-    }
+		stepper.on("kt.stepper.previous", function (stepper) {
+			stepper.goPrevious(); // go previous step
+		});
+	}
 
-    return {
-        init: function () {
-            div_element = document.querySelector('#onu_stepper_activate');
-            handle();
-        }
-    }
+	return {
+		init: function () {
+			div_element = document.querySelector('#onu_stepper_activate');
+			handle();
+		}
+	}
 }();
