1ab2e4e9d22486411a61b0d81d2c4103cc4ff3d7
gguerreroi <gguerrero@sonivitac.com>
2023-07-07 19:41:16 -0600

    - fix customers by id

--- public/assets/js/custom/customers/customers-byid.js
+++ public/assets/js/custom/customers/customers-byid.js
@@ -1,15 +1,14 @@
 "use strict";
-
 const KTCustomer = function () {
     let form_customer;
-
+    let cif;
     function handle() {
 
     }
 
     return {
         init: function () {
-
+            cif = document.getElementById('CODCLIENTE').value;
             handle();
         },
         initNewCustomer: function () {
