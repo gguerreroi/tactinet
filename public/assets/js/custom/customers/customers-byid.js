eceb3636ba779f24d99b174beb32dfa94f36306f
Gabriel Guerrero <47202574+gguerreroi@users.noreply.github.com>
2023-07-10 11:43:17 -0600

    add enable catv refactor js by-id.ejs

--- public/assets/js/custom/customers/customers-byid.js
+++ public/assets/js/custom/customers/customers-byid.js
@@ -2,13 +2,137 @@
 const KTCustomer = function () {
     let form_customer;
     let cif;
+    let select_codnacionalidad;
+    let select_codprofesion;
+    let select_codocupacion;
+    let select_condicionmigratoria;
+    let select_codcondicionvivienda;
+    let select_codgeopaisnacimiento;
+    let select_codgeodeptonacimiento;
+    let select_codgeomunnacimiento;
+    let select_codgeoaldnacimiento;
+    let select_codgeopaisresidencia;
+    let select_codgeodepresidencia;
+    let select_codgeomunresidencia;
+    let select_codgeoaldresidencia;
+    let input_apellidocasada;
+    let input_codestadocivil;
+    let input_codgenero;
     function handle() {
+        toggleApellidoCasada();
+
+
+    }
+
+    function toggleApellidoCasada(){
+        input_apellidocasada.prop('readonly', true);
+        if (input_codgenero.val() === 'F')
+            if (input_codestadocivil.val() === 'C')
+                input_apellidocasada.prop('readonly', false);
+
 
     }
 
     return {
         init: function () {
+
             cif = document.getElementById('CODCLIENTE').value;
+            input_apellidocasada = $("#T_APELLIDO");
+            input_codgenero = $("#CODGENERO");
+            input_codestadocivil = $("#CODESTADOCIVIL");
+
+            select_codnacionalidad = $("#CODNACIONALIDAD").select2({
+                placeholder: 'Seleccione una opción',
+                ajax: {
+                    url: `${url}/catalogue/clientes.nacionalidad/codnacionalidad/txtnacionalidad`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            });
+
+            select_codprofesion = $("#CODPROFESION").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/clientes.profesion/codprofesion/txtprofesion`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_codocupacion = $("#CODOCUPACION").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/clientes.ocupacion/codocupacion/txtocupacion`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_condicionmigratoria = $("#CONDICIONMIGRATORIA").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/clientes.condicionmigratoria/codcondicionmigra/descripcion`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_codcondicionvivienda = $("#CODCONDICIONVIVIENDA").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/clientes.condicionvivienda/codcondicionvivienda/txtcondicionvivienda`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item){
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_codgeopaisnacimiento = $("#CODGEOPAISNACIMIENTO").select2({
+                placeholder: 'Seleccione una opción',
+                ajax: {
+                    url: `${url}/catalogue/financiero.vw_paises/ISO31661N/txtvalue`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_codgeodeptonacimiento = $("#CODGEODEPNACIMIENTO").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/financiero.vw_deptos/codine/txtvalue`,
+                    dataType: 'json'
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
+
+            select_codgeomunnacimiento = $("#CODGEOMUNNACIMIENTO").select2({
+                placeholder: "Seleccione una opción",
+                ajax: {
+                    url: `${url}/catalogue/financiero.vw_mun/codine/txtvalue`,
+                    type: 'get',
+                    dataType: 'json',
+                    data: {
+                        cname: 'CODINEP',
+                        cvalue: select_codgeodeptonacimiento.val()
+                    }
+                },
+                templateSelection: function (item) {
+                    return $('<div class="mt-2">' + item.text + '</div>')
+                }
+            })
             handle();
         },
         initNewCustomer: function () {
--- src/controllers/catalogue.controllers.js
+++ src/controllers/catalogue.controllers.js
@@ -9,6 +9,22 @@ import config from "../config/config";
 export async function get_catalogue(req, res) {
     const {Username, Database, Password} = get_credentials(req);
     const {view, cod, str} = req.params;
+    const {term, _type, q} = req.query;
+    const {colname, colvalue} = req.body;
+    let whr = '';
+
+    if (term !== undefined && term !== '')
+        whr = `(cast(${cod} as varchar) + ${str}) like '%${term}%'`
+
+    if (colname !== undefined && colname !== '')
+        if (colvalue !== undefined && colvalue !== '')
+            whr += ` and ${colname} like '%${colvalue}%'`
+
+    if (whr.length > 3)
+        whr = 'WHERE ' + whr
+
+    console.log('body: ', req.body, 'whr', whr, 'query: ', req.query, 'params: ', req.params);
+
     let Connection = null
     try {
         Connection = await get_connection(Username, Password, `${config.DB.HOST}`, `PLR00${Database}`);
@@ -18,17 +34,17 @@ export async function get_catalogue(req, res) {
 
         const stmt = await Connection.request()
 
-        let sql = `SELECT ${cod} as id, ${str} as text
-                   FROM ${view}`;
+        let sql = `SELECT ${cod} as id, cast(${cod} as varchar) + ' ' + ${str} as text
+                   FROM ${view} ${whr}`;
 
         stmt.query(sql, (err, result) => {
             if (err) {
-                res.status(500).send(json_out('500', 'Error in controller getAll', err));
+                res.status(500).send(json_out('500', 'Error in query get_catalogue', err));
             } else {
                 res.status(200).send({results: result.recordset});
             }
         });
     } catch (e) {
-        res.status(500).send(json_out('500', 'Error in controller getAll [', e));
+        res.status(500).send(json_out('500', 'Error in controller get_catalogue', e));
     }
 }
